062d97107c34d6f12f03fb32497e19869d101c63
wqs1999 <80797792+wqs1999@users.noreply.github.com>
2024-11-26 21:28:46 +0800

    Add files via upload

---
+++ MQCNN-4qubits-2class-CIFAR-same-5filter-V7/MQCNNConvolutionLayer.py
@@ -0,0 +1,166 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+torch.cuda.set_device(1)
+import Circuit
+
+import pennylane as qml
+import datetime
+import matplotlib.pyplot as plt
+
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+class MQCNN_Conv2d(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, input_size: int = 32, pooling_num: int = 2) -> None:
+        super(MQCNN_Conv2d, self).__init__()
+        self.in_channels = in_channels  # 输入通道数
+        self.out_channels = out_channels  # 输出通道数
+        self.kernel_size = kernel_size  # 卷积核大小 kernel_size*kernel_size
+        self.stride = stride  # 步长
+        self.input_size = input_size
+        self.dev = qml.device("default.qubit", wires=self.kernel_size ** 2)
+        # self.dev = qml.device("lightning.qubit", wires=self.kernel_size ** 2)
+
+        self.pooling_num = pooling_num
+
+        # region 定义卷积核参数
+        self.kernels = []
+        self.attention = []
+        self.out = self.init_out()
+        for i in range(self.out_channels):
+            kernels_temp = []
+            for j in range(self.in_channels):
+                kernels_temp.append(self.select_filter(i))
+            kernels_temp = nn.ModuleList(kernels_temp)
+            self.kernels.append(kernels_temp)
+            self.attention.append(self.select_Attention(self.pooling_num))
+
+        self.kernels = nn.ModuleList(self.kernels)
+        self.attention = nn.ModuleList(self.attention)
+
+        # print(self.kernels)
+        # endregion
+
+        # region 归一化
+
+        self.bactnorm = nn.BatchNorm2d(num_features=self.in_channels*self.kernel_size**2)
+        self.avg_pool = nn.AdaptiveMaxPool2d((self.pooling_num, self.pooling_num))  # 全局平均池化
+        # endregion
+
+    # 初始化out
+    def init_out(self):
+        out = []
+        for i in range(self.out_channels):
+            out.append([])
+        return out
+
+    # 选择kernels
+    def select_filter(self, i):
+        if i % 5 == 0:
+            weight_shapes_Ran = {"weights": (7,)}
+            return qml.qnn.TorchLayer(qml.QNode(Circuit.circuit_Ran_Ry_4qubits, self.dev, diff_method="best"), weight_shapes_Ran)
+        if i % 5 == 1:
+            weight_shapes_Line = {"weights": (7,)}
+            return qml.qnn.TorchLayer(qml.QNode(Circuit.circuit_Line_Ry_4qubits, self.dev, diff_method="best"), weight_shapes_Line)
+        if i % 5 == 2:
+            weight_shapes_Ring = {"weights": (8,)}
+            return qml.qnn.TorchLayer(qml.QNode(Circuit.circuit_Ring_Ry_4qubits, self.dev, diff_method="best"), weight_shapes_Ring)
+        if i % 5 == 3:
+            weight_shapes_DoubleRing = {"weights": (16,)}
+            return qml.qnn.TorchLayer(qml.QNode(Circuit.circuit_DoubleRing_Ry_4qubits, self.dev, diff_method="best"), weight_shapes_DoubleRing)
+        if i % 5 == 4:
+            weight_shapes_BlockRing = {"weights": (12,)}
+            return qml.qnn.TorchLayer(qml.QNode(Circuit.circuit_BlockRing_Ry_4qubits, self.dev, diff_method="best"), weight_shapes_BlockRing)
+
+    # 生成注意力模块
+    def select_Attention(self, pooling_num):
+        attention_layer = torch.nn.Sequential(
+            nn.Linear(in_features=self.pooling_num ** 2 * self.in_channels * self.kernel_size ** 2, out_features=self.in_channels * self.kernel_size ** 2, bias=True),
+            nn.ReLU(),
+            nn.Linear(self.in_channels * self.kernel_size ** 2, self.in_channels * self.kernel_size ** 2, bias=True),
+            nn.Sigmoid())
+        return attention_layer
+
+    # 执行kernels
+    def CarryParallelKernel(self, kernels, inputs, index):
+        # torch.Size([128, 3, 4]) inputs
+
+        out = []
+        for i in range(len(inputs[0])):
+            out.append(kernels[i](inputs[:, i:i + 1, ]))
+        # out 3 [128,1,4]
+        out = torch.stack(out, dim=1).reshape([len(inputs), len(inputs[0]) * len(inputs[0][0])])
+        self.out[index].append(out)
+
+    # 向前传播
+    def forward(self, inputs):
+        # 2 2 3 3
+        # output_cpu = inputs.to("cpu")
+        # image = output_cpu.detach()[0].permute(1, 2, 0).numpy()
+        # plt.imshow(image)
+        # plt.show()
+
+        # region configure
+        batch_size = len(inputs)
+        in_channel_size = len(inputs[0])
+        out_channel_size = self.out_channels
+        new_image_length = len(inputs[0][0]) - self.kernel_size + 1
+        new_image_width = len(inputs[0][0][0]) - self.kernel_size + 1
+        # endregion
+
+        # region 特征提取
+        for q in range(new_image_length):
+            for p in range(new_image_width):
+                input_view = inputs[:, :, q:q + 2, p:p + 2].reshape([batch_size, in_channel_size, self.kernel_size ** 2])
+                for index in range(len(self.kernels)):
+                    self.CarryParallelKernel(kernels=self.kernels[index], inputs=input_view, index=index)
+
+        output = []
+        for index in range(len(self.kernels)):
+            output.append(torch.stack(self.out[index], dim=2).squeeze().reshape([batch_size, self.in_channels*self.kernel_size**2, new_image_length, new_image_length]))  # torch.Size([20, 12, 225])
+        self.out = self.init_out()
+        # endregion
+
+        # region 注意力计算
+        b, c, f, g = output[0].size()  # 输入特征图的维度[20, 12, 15, 15]
+        for i in range(self.out_channels):
+            attention_weight = self.attention[i](self.avg_pool(output[i]).view(b, c*self.pooling_num**2)).view(b, c, 1, 1)
+            output[i] = torch.sum(self.bactnorm(output[i] * attention_weight.expand_as(output[i])), dim=1)  # 重标定
+        # endregion
+
+        output = torch.stack(output, dim=1).squeeze()
+        output = output.reshape([batch_size, out_channel_size, new_image_length, new_image_width])
+        # output_cpu = output.to("cpu")
+        # print("output[0][0]:{}".format(output[0][0]))
+        # image = output_cpu.detach()[0].permute(1, 2, 0).numpy()
+        # for i in range(len(image[0][0])):
+        #     plt.imshow(image[:, :, i:i + 1].squeeze(), cmap='gray')
+        #     plt.show()
+        # print(output.shape)
+        return output
+
+
+if __name__ == '__main__':
+    # m = MQCNN_Conv2d(1, 2, 3, 4)
+    # print(datetime.datetime.now())
+    # print(m(torch.tensor([0.5, 0.5, 0.5, 0.5])))
+    # print(m.state_dict())
+    # torch.save(m.state_dict(), "./SAVE_PATH/TrainingModel")
+    # print(datetime.datetime.now())
+    # 2 3 2
+    tensor1 = torch.tensor([[[1, 2], [3, 4], [5, 6]],
+                            [[7, 8], [9, 10], [11, 12]]], dtype=int)
+    # 2 3
+    tensor2 = torch.tensor([[1, 2, 3],
+                            [4, 5, 6]], dtype=int)
+    tensor2 = tensor2.reshape([2, 3, 1])
+    print(tensor1)
+    print(tensor2.expand_as(tensor1))
+
+    # 如果你想在维度0（行）上对多个张量进行求和，可以使用torch.sum
+    print(tensor1 * tensor2)
+    sum_result = torch.sum(tensor1 * tensor2, dim=1)
+    print(sum_result)
---
+++ MQCNN-4qubits-2class-CIFAR-same-5filter-V7/main_MQCNN.py
@@ -0,0 +1,120 @@
+import torch
+from torch import optim
+from MQCNNConvolutionLayer import MQCNN_Conv2d
+import Tool
+# In[]
+
+# torch.set_default_device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    input_size = 10
+    TrainLoader, TestLoader, classes = Tool.DataLoader_1280(batch_size=128, num_workers=32, input_size=input_size, Train_num=640, Test_num=100, category=10)  # 加载数据
+    # Net = MQCNN_Conv2d(in_channels=3, out_channels=5, kernel_size=2, input_size=input_size)#实例化模型
+
+    Net = torch.nn.Sequential(
+        MQCNN_Conv2d(in_channels=3, out_channels=5, kernel_size=2, input_size=input_size),
+        torch.nn.MaxPool2d(kernel_size=2),
+        torch.nn.BatchNorm2d(num_features=5),
+        torch.nn.Flatten(start_dim=1, end_dim=3),
+        # torch.nn.Dropout(p=0.5),
+        torch.nn.Linear(in_features=5 * ((input_size - 1) // 2) ** 2, out_features=32),
+        torch.nn.ReLU(),
+        torch.nn.Linear(in_features=32, out_features=10),
+    )
+    Net = Net.to(device=device)
+    print(Net)
+    # for p in Net.parameters():
+    #     print(p.numel())
+    # total_params = sum(p.numel() for p in Net.parameters() if p.requires_grad)
+    # print(f'Total number of parameters: {total_params}') # 15825
+
+    criterion = torch.nn.CrossEntropyLoss().to(device)  # 定义损失函数
+    # optimizer = optim.SGD(Net.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(Net.parameters(), lr=0.001)
+
+    accuracies = []
+    test_loss = []
+
+    torch.set_num_threads(64)  # 设置线程数为 8，以加速训练过程。
+    for epoch in range(50):  # 指定训练的轮数为 2 轮（epoch），即遍历整个数据集两次。
+        running_loss = 0.0  # 记录当前训练阶段的损失值
+        for i, data in enumerate(TrainLoader, 0):
+            # 输入数据
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # 梯度清零
+            optimizer.zero_grad()  # 每个 batch 开始时，将优化器的梯度缓存清零，以避免梯度累积
+            # optimizer_Adam.zero_grad()
+
+            # forward + backward
+            outputs = Net(inputs)
+            # print(outputs[0])
+
+
+            loss = criterion(outputs, labels)  # 进行前向传播，然后计算损失函数 loss
+            loss.backward()  # 自动计算损失函数相对于模型参数的梯度
+            # 更新参数
+            # print('[epoch:%d, step:%5d] loss: %f' % (epoch + 1, i + 1, loss.item()))
+            optimizer.step()  # 使用优化器 optimizer 来更新模型的权重和偏置，以最小化损失函数
+            # optimizer_Adam.step()
+            # for name, param in Net.named_parameters():
+            #     if param.requires_grad:  # 检查参数是否需要梯度
+            #         print(f"{name}: {param.grad}")
+            # 打印log信息
+            # loss 是一个scalar,需要使用loss.item()来获取数值，不能使用loss[0]
+            running_loss += loss.item()
+            # print('[epoch:%d, step:%5d] loss: %f' % (epoch + 1, i + 1, running_loss))
+            # running_loss = 0.0
+            if i % 10 == 9:  # 每10个batch打印一下训练状态
+                print('[epoch:%d, step:%5d] loss: %f' % (epoch + 1, i + 1, running_loss))
+                loss_copy = running_loss
+                running_loss = 0.0
+
+        correct = 0  # 预测正确的图片数
+        total = 0  # 总共的图片数
+
+        # 使用 torch.no_grad() 上下文管理器，表示在测试过程中不需要计算梯度，以提高速度和节约内存
+        with torch.no_grad():
+            for data in TestLoader:
+                images, labels = data
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = Net(images)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum()
+
+        print('10000张测试集中的准确率为: %.4f' % (correct / total))
+        # torch.save(Net.state_dict(), "./Models_V1_02/TrainingModel{}".format(epoch))
+
+        accuracy = correct / total
+        accuracies.append(accuracy.item())
+        running_loss1 = 0.0
+        with torch.no_grad():
+            for data in TestLoader:
+                images, labels = data
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = Net(images)
+                loss = criterion(outputs, labels)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                running_loss1 += loss.item()
+        print('10000张测试集中损失值为: ', running_loss1)
+        test_loss.append(running_loss1)
+        torch.save(Net, "./Models_V1/model_{}.pt".format(epoch))
+
+    print('Finished Training')
+    with open("./acc_and_loss/acc_02.txt", "w") as f:
+        for accuracy in accuracies:
+            f.write(str(accuracy) + '\n')
+    with open("./acc_and_loss/loss_02.txt", "w") as f:
+        for loss_copy in test_loss:
+            f.write(str(loss_copy) + '\n')
+    # Image._show(show((data + 1) / 2).resize((200, 200)))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
