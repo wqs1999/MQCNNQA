c50cb00884f675a86cac11fcfede1f8ff9062b23
wqs1999 <80797792+wqs1999@users.noreply.github.com>
2024-11-26 21:02:39 +0800

    Add files via upload

---
+++ MQCNN-4qubits-3class-CIFAR/Circuit.py
@@ -0,0 +1,326 @@
+import pennylane as qml
+import torch
+
+# 随机型
+def circuit_Ran_Rx_4qubits(inputs, weights):
+    # print(inputs)
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RX(weights[0], wires=0)
+    qml.RX(weights[1], wires=1)
+    qml.RX(weights[2], wires=2)
+    qml.RX(weights[3], wires=3)
+    qml.CRX(weights[4], wires=[1, 0])
+    qml.CRX(weights[5], wires=[2, 1])
+    qml.CRX(weights[6], wires=[1, 3])
+    # endregion
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+
+def circuit_Ran_Rz_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RZ(weights[0], wires=0)
+    qml.RZ(weights[1], wires=1)
+    qml.RZ(weights[2], wires=2)
+    qml.RZ(weights[3], wires=3)
+    qml.CRZ(weights[4], wires=[1, 0])
+    qml.CRZ(weights[5], wires=[2, 1])
+    qml.CRZ(weights[6], wires=[1, 3])
+    # endregion
+    return [qml.expval(qml.PauliY(i)) for i in range(4)]
+
+
+def circuit_Ran_Ry_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RY(weights[0], wires=0)
+    qml.RY(weights[1], wires=1)
+    qml.RY(weights[2], wires=2)
+    qml.RY(weights[3], wires=3)
+    qml.CRY(weights[4], wires=[1, 0])
+    qml.CRY(weights[5], wires=[2, 1])
+    qml.CRY(weights[6], wires=[1, 3])
+    # endregion
+    return qml.expval(qml.PauliZ(1))
+
+# 线型
+def circuit_Line_Rx_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RX(weights[0], wires=0)
+    qml.RX(weights[1], wires=1)
+    qml.RX(weights[2], wires=2)
+    qml.RX(weights[3], wires=3)
+    qml.CRX(weights[4], wires=[1, 0])
+    qml.CRX(weights[5], wires=[2, 3])
+    qml.CRX(weights[6], wires=[2, 1])
+
+    # endregion
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+
+def circuit_Line_Ry_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RY(weights[0], wires=0)
+    qml.RY(weights[1], wires=1)
+    qml.RY(weights[2], wires=2)
+    qml.RY(weights[3], wires=3)
+    qml.CRY(weights[4], wires=[1, 0])
+    qml.CRY(weights[5], wires=[2, 3])
+    qml.CRY(weights[6], wires=[2, 1])
+    # endregion
+    return qml.expval(qml.PauliZ(1))
+
+
+def circuit_Line_Rz_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RZ(weights[0], wires=0)
+    qml.RZ(weights[1], wires=1)
+    qml.RZ(weights[2], wires=2)
+    qml.RZ(weights[3], wires=3)
+    qml.CRZ(weights[4], wires=[1, 0])
+    qml.CRZ(weights[5], wires=[2, 3])
+    qml.CRZ(weights[6], wires=[2, 1])
+    # endregion
+    return [qml.expval(qml.PauliY(i)) for i in range(4)]
+
+# 环型
+def circuit_Ring_Rx_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RX(weights[0], wires=0)
+    qml.RX(weights[1], wires=1)
+    qml.RX(weights[2], wires=2)
+    qml.RX(weights[3], wires=3)
+    qml.CRX(weights[4], wires=[1, 0])
+    qml.CRX(weights[5], wires=[2, 1])
+    qml.CRX(weights[6], wires=[3, 2])
+    qml.CRX(weights[7], wires=[0, 3])
+
+    # endregion
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+
+def circuit_Ring_Ry_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RY(weights[0], wires=0)
+    qml.RY(weights[1], wires=1)
+    qml.RY(weights[2], wires=2)
+    qml.RY(weights[3], wires=3)
+    qml.CRY(weights[4], wires=[1, 0])
+    qml.CRY(weights[5], wires=[2, 1])
+    qml.CRY(weights[6], wires=[3, 2])
+    qml.CRY(weights[7], wires=[0, 3])
+    # endregion
+    return qml.expval(qml.PauliZ(1))
+
+
+def circuit_Ring_Rz_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RZ(weights[0], wires=0)
+    qml.RZ(weights[1], wires=1)
+    qml.RZ(weights[2], wires=2)
+    qml.RZ(weights[3], wires=3)
+    qml.CRZ(weights[4], wires=[1, 0])
+    qml.CRZ(weights[5], wires=[2, 1])
+    qml.CRZ(weights[6], wires=[3, 2])
+    qml.CRZ(weights[7], wires=[0, 3])
+    # endregion
+    return [qml.expval(qml.PauliY(i)) for i in range(4)]
+
+# 异构环型
+def circuit_DoubleRing_Rx_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RX(weights[0], wires=0)
+    qml.RX(weights[1], wires=1)
+    qml.RX(weights[2], wires=2)
+    qml.RX(weights[3], wires=3)
+    qml.CRX(weights[4], wires=[3, 0])
+    qml.CRX(weights[5], wires=[2, 3])
+    qml.CRX(weights[6], wires=[1, 2])
+    qml.CRX(weights[7], wires=[0, 1])
+
+    qml.RX(weights[8], wires=0)
+    qml.RX(weights[9], wires=1)
+    qml.RX(weights[10], wires=2)
+    qml.RX(weights[11], wires=3)
+    qml.CRX(weights[12], wires=[3, 2])
+    qml.CRX(weights[13], wires=[0, 3])
+    qml.CRX(weights[14], wires=[2, 1])
+    qml.CRX(weights[15], wires=[1, 0])
+    # endregion
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+
+def circuit_DoubleRing_Ry_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RY(weights[0], wires=0)
+    qml.RY(weights[1], wires=1)
+    qml.RY(weights[2], wires=2)
+    qml.RY(weights[3], wires=3)
+    qml.CRY(weights[4], wires=[3, 0])
+    qml.CRY(weights[5], wires=[2, 3])
+    qml.CRY(weights[6], wires=[1, 2])
+    qml.CRY(weights[7], wires=[0, 1])
+
+    qml.RY(weights[8], wires=0)
+    qml.RY(weights[9], wires=1)
+    qml.RY(weights[10], wires=2)
+    qml.RY(weights[11], wires=3)
+    qml.CRY(weights[12], wires=[3, 2])
+    qml.CRY(weights[13], wires=[0, 3])
+    qml.CRY(weights[14], wires=[2, 1])
+    qml.CRY(weights[15], wires=[1, 0])
+    # endregion
+    return qml.expval(qml.PauliZ(1))
+
+
+def circuit_DoubleRing_Rz_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RZ(weights[0], wires=0)
+    qml.RZ(weights[1], wires=1)
+    qml.RZ(weights[2], wires=2)
+    qml.RZ(weights[3], wires=3)
+    qml.CRZ(weights[4], wires=[3, 0])
+    qml.CRZ(weights[5], wires=[2, 3])
+    qml.CRZ(weights[6], wires=[1, 2])
+    qml.CRZ(weights[7], wires=[0, 1])
+
+    qml.RZ(weights[8], wires=0)
+    qml.RZ(weights[9], wires=1)
+    qml.RZ(weights[10], wires=2)
+    qml.RZ(weights[11], wires=3)
+    qml.CRZ(weights[12], wires=[3, 2])
+    qml.CRZ(weights[13], wires=[0, 3])
+    qml.CRZ(weights[14], wires=[2, 1])
+    qml.CRZ(weights[15], wires=[1, 0])
+    # endregion
+    return [qml.expval(qml.PauliY(i)) for i in range(4)]
+
+# 区块环型
+def circuit_BlockRing_Rx_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RX(weights[0], wires=0)
+    qml.RX(weights[1], wires=1)
+    qml.RX(weights[2], wires=2)
+    qml.RX(weights[3], wires=3)
+    qml.CRX(weights[4], wires=[2, 0])
+    qml.CRX(weights[5], wires=[3, 1])
+
+    qml.CRX(weights[6], wires=[0, 1])
+    qml.CRX(weights[7], wires=[1, 0])
+    qml.CRX(weights[8], wires=[2, 3])
+    qml.CRX(weights[9], wires=[3, 2])
+
+    qml.CRX(weights[10], wires=[0, 2])
+    qml.CRX(weights[11], wires=[1, 3])
+
+    # endregion
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+
+def circuit_BlockRing_Ry_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RY(weights[0], wires=0)
+    qml.RY(weights[1], wires=1)
+    qml.RY(weights[2], wires=2)
+    qml.RY(weights[3], wires=3)
+    qml.CRY(weights[4], wires=[2, 0])
+    qml.CRY(weights[5], wires=[3, 1])
+
+    qml.CRY(weights[6], wires=[0, 1])
+    qml.CRY(weights[7], wires=[1, 0])
+    qml.CRY(weights[8], wires=[2, 3])
+    qml.CRY(weights[9], wires=[3, 2])
+
+    qml.CRY(weights[10], wires=[0, 2])
+    qml.CRY(weights[11], wires=[1, 3])
+    # endregion
+    return qml.expval(qml.PauliZ(0))
+
+
+def circuit_BlockRing_Rz_4qubits(inputs, weights):
+    # region encoder
+    qml.AngleEmbedding(features=inputs, wires=range(4), rotation='Y')
+    # endregion
+
+    # region anataz
+    qml.RZ(weights[0], wires=0)
+    qml.RZ(weights[1], wires=1)
+    qml.RZ(weights[2], wires=2)
+    qml.RZ(weights[3], wires=3)
+    qml.CRZ(weights[4], wires=[2, 0])
+    qml.CRZ(weights[5], wires=[3, 1])
+
+    qml.CRZ(weights[6], wires=[0, 1])
+    qml.CRZ(weights[7], wires=[1, 0])
+    qml.CRZ(weights[8], wires=[2, 3])
+    qml.CRZ(weights[9], wires=[3, 2])
+
+    qml.CRZ(weights[10], wires=[0, 2])
+    qml.CRZ(weights[11], wires=[1, 3])
+    # endregion
+    return [qml.expval(qml.PauliY(i)) for i in range(4)]
+
+if __name__ == "__main__":
+    n_wires = 20
+    n_layers = 10
+
+    dev = qml.device('default.qubit', wires=n_wires)
+
+    params_shape = (10,)
+    print(type(params_shape))  # (10, 20, 3)<class 'tuple'>
+    params = torch.rand(params_shape)
+    print(params[0])
---
+++ MQCNN-4qubits-3class-CIFAR/TestLoss.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+from MQCNNConvolutionLayer import MQCNN_Conv2d
+import Tool
+
+
+
+input_size = 10
+
+TrainLoader, TestLoader, classes = Tool.DataLoader_1280(batch_size=128, num_workers=32, input_size=input_size, category=10)
+#TrainLoader, TestLoader, classes = Tool.DataLoad(batch_size=50, num_workers=32, input_size=input_size)  # 加载数据
+model = torch.nn.Sequential(
+    MQCNN_Conv2d(in_channels=3, out_channels=5, kernel_size=2, input_size=input_size),
+    torch.nn.MaxPool2d(kernel_size=2),
+    torch.nn.BatchNorm2d(num_features=5),
+    # MQCNN_Conv2d(in_channels=5, out_channels=10, kernel_size=2, input_size=((input_size - 1) // 2)),
+    # torch.nn.MaxPool2d(kernel_size=2),
+    # torch.nn.BatchNorm2d(num_features=10),
+    torch.nn.Flatten(start_dim=1, end_dim=3),
+    # torch.nn.Dropout(p=0.5),
+    torch.nn.Linear(in_features=5 * ((input_size - 1) // 2) ** 2, out_features=32),
+    torch.nn.ReLU(),
+    torch.nn.Linear(in_features=32, out_features=10),
+    )
+
+# 加载模型权重
+# 假设权重文件名为'model_weights.pth'
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+model_path = './Models_V1/'
+criterion = torch.nn.CrossEntropyLoss().to(device)  # 定义损失函数
+
+
+test_loss=[]
+def test_model(i):
+    correct = 0  # 预测正确的图片数
+    total = 0  # 总共的图片数
+    running_loss = 0.0  # 记录当前训练阶段的损失值
+    path = model_path+"model_"+str(i)+".pt"
+    print(path)
+    weight = torch.load(path)
+    for i in range(len(weight)):
+        model[i] = weight[i]
+    model.to(device)
+    count=0
+    with torch.no_grad():
+
+        for data in TestLoader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+            running_loss += loss.item()
+            count += 1
+        print('count:%d' % count)
+        print('10000张测试集中的准确率为: %.5f' % (correct / total))
+        print('10000张测试集中损失值为: ', running_loss)
+
+        test_loss.append(running_loss)
+for i in range(-1, 49):
+    test_model(i+1)
+
+# with open('./acc_and_loss/test_loss_128.txt', 'w') as f:
+#     for loss in test_loss:
+#         f.write(str(loss) + '\n')
